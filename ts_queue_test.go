eaa8bdef5231b754bc9844596bfe56f207ccc405
rutaka <rutaka.nashimo@gmail.com>
2023-12-17 18:54:18 +0100

    implement ts_queue - queue that tracks expiration time of items in the cache

---
+++ ts_queue_test.go
@@ -0,0 +1,22 @@
+package plrucache
+
+import (
+	"testing"
+	"time"
+)
+
+func TestIsAnyExpired(t *testing.T) {
+	q := newTSQ[string](1)
+	val := "val"
+    now := time.Now()
+	q.Push(val, now.Add(10*time.Second))
+	if got := q.IsAnyExpired(now); got {
+		t.Errorf("expected: %v, got: %v", false, got)
+	}
+	if got := q.IsAnyExpired(now.Add(11 * time.Second)); !got {
+		t.Errorf("expected: %v, got: %v", true, got)
+	}
+	if got := q.IsAnyExpired(now.Add(10 * time.Second)); !got {
+		t.Errorf("expected: %v, got: %v", true, got)
+	}
+}
